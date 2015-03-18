de5afe4f5e8af3eb3885dd59683d739e6a10b2c3
Alexander Kushko <kushko.alex@gmail.com>
2015-03-18 23:04:31 +0200

    --

--- public/scripts/newVisionProductions/elements/media/tmpls_media.js
+++ public/scripts/newVisionProductions/elements/media/tmpls_media.js
@@ -20,13 +20,23 @@
 
     tmpls.mediaItem = function (mediaItemData) {
         var controlsDescriptors = nv.settings.controlsDescriptors,
-            graphicsContent,
+            mediaContent,
             eventInfo = [],
             eventInfoClass = 'event-info';
 
+        if(mediaItemData.mediaType=='video'){
+            mediaContent={e: 'iframe', a:{width:'560', height:'338', src:mediaItemData.mediaSrc, frameborder:'0'}};
+        }else{
+            mediaContent = {
+                e: 'img',
+                a: {src: controlsDescriptors.site.contentImagesPath + mediaItemData.mediaSrc}
+            }
+        }
+
         return {
             c: 'media-item', C: [
-                {c: 'media-content',C:{e: 'iframe', a:{width:'560', height:'338', src:mediaItemData.mediaSrc, frameborder:'0'}}},
+                {c: 'media-content',C:mediaContent
+                },
                 {
                     c: 'description', C: [
                     {c: 'logo'},
