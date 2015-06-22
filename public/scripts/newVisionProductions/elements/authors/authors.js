c1a876f8d74741de5165f393b226202f95fef56b
Alexander Kushko <kushko.alex@gmail.com>
2015-06-23 01:30:04 +0300

    author page development v.1

--- public/scripts/newVisionProductions/elements/authors/authors.js
+++ public/scripts/newVisionProductions/elements/authors/authors.js
@@ -1,4 +1,4 @@
-NV.authors = function($parent){
+NV.authors = function ($parent) {
     var nv = this,
         global = nv.global,
         a9 = global.A9,
@@ -6,17 +6,47 @@ NV.authors = function($parent){
         settings = nv.settings,
         authorsData = settings.dataModels.authors,
         $fragment,
-        build;
+        $fragmentTag,
+        build,
+        buildTag,
+        itemsInRow = settings.controlsDescriptors.site.authorProductItemsInRowCount,
+        itemsCount = 0;
 
-    function buildMediaForm(authorsData){
+    function buildMediaForm(authorsData) {
         $fragment = global.document.createDocumentFragment();
+        $fragmentTag = global.document.createDocumentFragment();
+        //console.log(authorsData);
 
-        //a9.each(mediaData, function(mediaItem){
-        //    build = tp('mediaItem',mediaItem, $fragment)
-        //});
+        a9.each(authorsData, function (authorsDataItem) {
+
+            //console.log(authorsDataItem);
+
+            if (itemsCount % itemsInRow == 0) {
+                tp('clear', $fragment)
+            }
+
+            build = tp('author', authorsDataItem, $fragment);
+
+            itemsCount++;
+
+
+            var $tags = build.tags;
+
+            a9.each(authorsDataItem.tags, function (tag) {
+                //console.log(tag);
+
+                buildTag = tp('tag', tag, $fragmentTag);
+
+                $tags.appendChild($fragmentTag);
+            });
+
+        });
 
         $authors.appendChild($fragment);
     }
+
     var $authors = tp('authors', $parent).authorsBlocks;
+
+
     buildMediaForm(authorsData);
 };
