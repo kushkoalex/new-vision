4cd7e6361bb87820d05d165a42ab86dceea91b08
Alexander Kushko <kushko.alex@gmail.com>
2015-08-03 00:59:35 +0300

    ---

--- public/scripts/newVisionProductions/elements/authorProductsDetails/tmpls_authorProductsDetails.js
+++ public/scripts/newVisionProductions/elements/authorProductsDetails/tmpls_authorProductsDetails.js
@@ -10,11 +10,11 @@
         var subMenuModel = [
             {
                 text: l10n('authors_subMenu_aboutAuthor', 'firstUpper'),
-                link: a9.supplant(nv.settings.controlsDescriptors.site.authorAboutPageUrl,{artist:authorInfo.name})
+                link: a9.supplant(nv.settings.controlsDescriptors.site.authorAboutPageUrl, {artist: authorInfo.name})
             },
             {
                 text: l10n('authors_subMenu_authorProducts', 'firstUpper'),
-                link:a9.supplant( nv.settings.controlsDescriptors.site.authorProductsPageUrl,{artist:authorInfo.name}),
+                link: a9.supplant(nv.settings.controlsDescriptors.site.authorProductsPageUrl, {artist: authorInfo.name}),
                 isActive: true
             },
             {text: l10n('authors_subMenu_authorEvents', 'firstUpper')}
@@ -31,25 +31,33 @@
     };
 
 
-    tmpls.authorProductsDetailsForm = function(authorInfo){
+    tmpls.authorProductsDetailsForm = function (authorInfo) {
         var
             contentImagePath = nv.settings.controlsDescriptors.site.contentImagesPathAuthorsOrig,
             imagesContent = [],
-            authorImages = authorInfo.images;
+            products = nv.settings.dataModels.products;
+            ;
+
+            //authorImages = authorInfo.images;
 
 
-        for (var i = 0; i < authorImages.length; i++) {
+        for (var i = 0; i < products.length; i++) {
             imagesContent.push({
                 e: 'li',
-                C: {e: 'img', a: {src: contentImagePath + authorImages[i]}}
+                C: {e: 'img', a: {src: contentImagePath + products[i].photo}}
             })
         }
 
-        return {c:'bxsliderProductDetailsContainer', C:
-            {c: 'image-slider', C: {e: 'ul', c: 'bxsliderProductDetails', C: imagesContent}}
+        return {
+            c: 'bxsliderProductDetailsContainer', C: [{
+                c: 'productDetailsInfo', C: [
+                    {c: 'title', a: {id: 'productDescriptionTitle'}, t: ''},
+                    {c:'tags', a: {id: 'productDescriptionTags'}},
+                    {c:'price',a: {id: 'productDescriptionPrice'}}
+                ]
+            }, {c: 'image-slider', C: {e: 'ul', c: 'bxsliderProductDetails', C: imagesContent}}]
         }
     }
-
 
 
 }(NV));
--- public/vendor/scripts/init.js
+++ public/vendor/scripts/init.js
@@ -1,19 +1,80 @@
-$(function(){
+$(function () {
     $('.bxslider').bxSlider({
         minSlides: 3,
         maxSlides: 10,
         slideWidth: 400,
         slideMargin: 0,
-        moveSlides:1,
-        pager:false
+        moveSlides: 1,
+        pager: false
     });
 
+    var products = NV.settings.dataModels.products;
+    var tagsContainer = document.getElementById('productDescriptionTags');
+    var titleContainer = document.getElementById('productDescriptionTitle');
+    var  productDescriptionPrice = document.getElementById('productDescriptionPrice');
 
-    $('.bxsliderProductDetails').bxSlider({
+    productDetailsSlider = $('.bxsliderProductDetails').bxSlider({
         adaptiveHeight: true,
         mode: 'fade',
-        pager:false
+        pager: false,
+        onSliderLoad: function (currentIndex) {
+            // do funky JS stuff here
+            //alert('Slider has finished loading. Click OK to continue!');
+
+
+            //console.log(products);
+
+            titleContainer.innerHTML = '&laquo' + products[0].title + '&raquo';
+            productDescriptionPrice.innerHTML = products[0].price;
+
+            for (var i = 0; i < products[0].tags.length; i++) {
+                var item = document.createElement('span');
+                var att = document.createAttribute("class");
+                att.value = "product-description-tag";
+                item.setAttributeNode(att);
+
+                item.innerHTML = products[0].tags[i];
+                tagsContainer.appendChild(item);
+            }
+
+
+            //console.log(products[0].tags);
+
+
+            //productDetailsSlider.goToSlide(2);
+
+        },
+        onSlideAfter: function ($slideElement, oldIndex, newIndex) {
+            // do mind-blowing JS stuff here
+            //alert('A slide has finished transitioning. Bravo. Click OK to continue!');
+            if (products && products[newIndex] && products[newIndex].title) {
+                titleContainer.innerHTML = '&laquo' + products[newIndex].title + '&raquo';
+                productDescriptionPrice.innerHTML = products[newIndex].price;
+            }
+            else {
+                titleContainer.innerHTML = '';
+                productDescriptionPrice.innerHTML = products[newIndex].price;
+            }
+
+            tagsContainer.innerHTML = '';
+
+            if (products[newIndex] && products[newIndex].tags) {
+                for (var i = 0; i < products[newIndex].tags.length; i++) {
+                    var item = document.createElement('span');
+                    var att = document.createAttribute("class");
+                    att.value = "product-description-tag";
+                    item.setAttributeNode(att);
+
+                    item.innerHTML = products[newIndex].tags[i];
+                    tagsContainer.appendChild(item);
+                }
+            }
+
+            //console.log(oldIndex);
+            //console.log(newIndex);
+        }
     });
+
 
     $(window).scroll(function () {
         if ($(this).scrollTop() > 0) {
